8446c3e34b9348650be0658861df701722df6831
Thomas Campistron <thomas.campistron@huawei.com>
2020-05-01 18:16:36 +0200

    add day2 part2

--- day2/src/cuboid.rs
+++ day2/src/cuboid.rs
@@ -15,6 +15,25 @@ impl Cuboid {
     pub fn paper_needed(&self) -> usize {
         self.surface() + self.smallest_face()
     }
+
+    fn smallest_distances_around_sides(&self) -> usize {
+        *[
+            2 * self.0 + 2 * self.1,
+            2 * self.1 + 2 * self.2,
+            2 * self.0 + 2 * self.2,
+        ]
+        .iter()
+        .min()
+        .unwrap()
+    }
+
+    fn volume(&self) -> usize {
+        self.0 * self.1 * self.2
+    }
+
+    pub fn ribbon_needed(&self) -> usize {
+        self.smallest_distances_around_sides() + self.volume()
+    }
 }
 
 #[cfg(test)]
@@ -37,5 +56,23 @@ mod tests {
     fn test_paper_needed() {
         assert_eq!(Cuboid(2, 3, 4).paper_needed(), 58);
         assert_eq!(Cuboid(1, 1, 10).paper_needed(), 43);
+    }
+
+    #[test]
+    fn test_smallest_distances_around_sides() {
+        assert_eq!(Cuboid(2, 3, 4).smallest_distances_around_sides(), 10);
+        assert_eq!(Cuboid(1, 1, 10).smallest_distances_around_sides(), 4);
+    }
+
+    #[test]
+    fn test_volume() {
+        assert_eq!(Cuboid(2, 3, 4).volume(), 24);
+        assert_eq!(Cuboid(1, 1, 10).volume(), 10);
+    }
+
+    #[test]
+    fn test_ribbon_needed() {
+        assert_eq!(Cuboid(2, 3, 4).ribbon_needed(), 34);
+        assert_eq!(Cuboid(1, 1, 10).ribbon_needed(), 14);
     }
 }
