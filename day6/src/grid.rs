8538fc4b3878ba03634b823116628b3c6f55e2e3
irevoire <irevoire@hotmail.fr>
2020-05-03 14:42:41 +0200

    udate part1 to be compliant with part2

--- day6/src/grid.rs
+++ day6/src/grid.rs
@@ -1,26 +1,30 @@
 use crate::Coord;
 
 pub struct Grid {
-    data: Vec<Vec<bool>>,
+    data: Vec<Vec<usize>>,
 }
 
 impl Grid {
     pub fn new() -> Self {
         Self {
-            data: vec![vec![false; 1000]; 1000],
+            data: vec![vec![0; 1000]; 1000],
         }
     }
 
     pub fn toggle(&mut self, coord: Coord) {
-        self[&coord] = !self[&coord];
+        match self[&coord] {
+            0 => self[&coord] = 1,
+            1 => self[&coord] = 0,
+            _ => panic!("you mixed part 1 with part 2"),
+        }
     }
 
     pub fn turn_on(&mut self, coord: Coord) {
-        self[&coord] = true;
+        self[&coord] = 1;
     }
 
     pub fn turn_off(&mut self, coord: Coord) {
-        self[&coord] = false;
+        self[&coord] = 0;
     }
 
     pub fn through(&mut self, from: Coord, to: Coord, apply: impl Fn(&mut Self, Coord)) {
@@ -32,12 +36,12 @@ impl Grid {
     pub fn lit(&self) -> usize {
         Coord::new(0, 0)
             .to(Coord::new(999, 999))
-            .fold(0_usize, |acc, coord| acc + self[&coord] as usize)
+            .fold(0_usize, |acc, coord| acc + self[&coord])
     }
 }
 
 impl std::ops::Index<&Coord> for Grid {
-    type Output = bool;
+    type Output = usize;
 
     fn index(&self, index: &Coord) -> &Self::Output {
         assert!(index.x < 1000 && index.y < 1000);
