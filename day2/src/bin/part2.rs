15ba1565eca6f08787744fb971c2a544ddd4accf
Thomas Campistron <irevoire@hotmail.fr>
2020-05-06 01:22:40 +0200

    try some shit with the aoc repo

--- day2/src/bin/part2.rs
+++ day2/src/bin/part2.rs
@@ -1,20 +1,10 @@
 use day2::Cuboid;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
 
 fn main() {
-    let filename = std::env::args()
-        .skip(1)
-        .next()
-        .expect("give me the path to your program"); // Skiping the name of the binary
-
-    let file = File::open(filename).expect("Can’t open file");
-    let reader = BufReader::new(file);
+    let reader = aoc::parser::lines_from_args(1);
 
     let paper: usize = reader
-        .lines()
         .map(|line| {
-            let line = line.unwrap();
             let mut split = line.split('x');
             Cuboid(
                 split.next().unwrap().parse().unwrap(),
