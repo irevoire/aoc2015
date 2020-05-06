866144b8b1154d5643886e1fa54e4b8218ad2e4a
Thomas Campistron <irevoire@hotmail.fr>
2020-05-06 11:17:31 +0200

    add day9 part2

---
+++ day9/src/bin/part2.rs
@@ -0,0 +1,35 @@
+use itertools::Itertools;
+
+fn main() {
+    let mut map = std::collections::HashMap::new();
+    let mut all_cities = std::collections::HashSet::new();
+
+    for line in aoc::parser::lines_from_args(1) {
+        let line: Vec<&str> = line.split("=").map(str::trim).collect();
+        let (cities, distance) = (line[0], line[1]);
+        let distance: usize = distance.parse().unwrap();
+        let cities: Vec<&str> = cities.split("to").map(str::trim).collect();
+        let (left, right) = (cities[0], cities[1]);
+
+        map.insert((left.to_string(), right.to_string()), distance);
+        map.insert((right.to_string(), left.to_string()), distance);
+
+        // could be better
+        all_cities.insert(left.to_string());
+        all_cities.insert(right.to_string());
+    }
+
+    let distance: usize = all_cities
+        .iter()
+        .permutations(all_cities.len())
+        .map(|all_cities| {
+            all_cities
+                .windows(2)
+                .map(|arr| map.get(&(arr[0].clone(), arr[1].clone())).unwrap())
+                .sum::<usize>()
+        })
+        .max()
+        .unwrap();
+
+    println!("the longest path is: {}", distance);
+}
