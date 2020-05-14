ee94fe943c38f5959bd33c3ed45f2da45c51c0f9
Thomas Campistron <irevoire@hotmail.fr>
2020-05-14 09:19:13 +0200

    add day17 part1

---
+++ day17/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let containers: Vec<usize> = aoc::parser::lines_from_args(1)
+        .map(|line| line.parse().unwrap())
+        .collect();
+    let mut used = vec![false; containers.len()];
+    let mut total = 0;
+
+    loop {
+        increment(&mut used);
+        let t = total_capacity(&containers, &used);
+        if t == 150 {
+            total += 1;
+        } else if t == 0 {
+            break;
+        }
+    }
+
+    println!("There is {} possibilities", total);
+}
+
+fn increment(used: &mut [bool]) {
+    for el in used {
+        if *el == true {
+            *el = false;
+        } else {
+            *el = true;
+            break;
+        }
+    }
+}
+
+fn total_capacity(containers: &[usize], used: &[bool]) -> usize {
+    used.iter()
+        .zip(containers)
+        .filter(|(used, _)| **used)
+        .map(|(_, capacity)| capacity)
+        .sum()
+}
