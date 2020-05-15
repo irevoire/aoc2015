20b6d0deb66e2d9a4c03db52e2847cab8787b582
Thomas Campistron <irevoire@hotmail.fr>
2020-05-15 13:11:41 +0200

    add day18 part1

---
+++ day18/src/bin/part1.rs
@@ -0,0 +1,39 @@
+use aoc::{Coord, Grid};
+
+fn main() {
+    let grid: Vec<Vec<bool>> = aoc::parser::lines_from_args(1)
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '#' => true,
+                    '.' => false,
+                    c => panic!("unknown char {}", c),
+                })
+                .collect::<Vec<bool>>()
+        })
+        .collect();
+
+    let mut grid = aoc::Grid::from(grid);
+
+    for _ in 0..100 {
+        grid = update_grid(&grid);
+    }
+
+    let total: usize = grid.iter().map(|b| *b as usize).sum();
+
+    println!("There is a total of {} light on", total);
+}
+
+fn update_grid(grid: &Grid<bool>) -> Grid<bool> {
+    let mut res = grid.clone();
+
+    for coord in Coord::at(0, 0).to(Coord::at(99, 99)).unwrap() {
+        let n = day18::neighbours(coord, &grid);
+        match (grid[coord], n) {
+            (true, 2) | (true, 3) | (false, 3) => res[coord] = true,
+            _ => res[coord] = false,
+        }
+    }
+
+    res
+}
---
+++ day18/src/lib.rs
@@ -0,0 +1,61 @@
+use aoc::{Coord, Direction::*, Grid};
+
+pub fn neighbours(coord: Coord<usize>, grid: &Grid<bool>) -> usize {
+    match (coord.x, coord.y) {
+        (0, 0) => {
+            grid[coord + East] as usize
+                + grid[coord + South] as usize
+                + grid[coord + South + East] as usize
+        }
+        (99, 99) => {
+            grid[coord + West] as usize
+                + grid[coord + North] as usize
+                + grid[coord + North + West] as usize
+        }
+        (0, 99) => {
+            grid[coord + North] as usize
+                + grid[coord + North + East] as usize
+                + grid[coord + East] as usize
+        }
+        (99, 0) => {
+            grid[coord + West] as usize
+                + grid[coord + South] as usize
+                + grid[coord + West + South] as usize
+        }
+        (0, _) => {
+            grid.through(coord + North, coord + South + East)
+                .unwrap()
+                .map(|b| *b as usize)
+                .sum::<usize>()
+                - grid[coord] as usize
+        }
+        (99, _) => {
+            grid.through(coord + North + West, coord + South)
+                .unwrap()
+                .map(|b| *b as usize)
+                .sum::<usize>()
+                - grid[coord] as usize
+        }
+        (_, 0) => {
+            grid.through(coord + West, coord + South + East)
+                .unwrap()
+                .map(|b| *b as usize)
+                .sum::<usize>()
+                - grid[coord] as usize
+        }
+        (_, 99) => {
+            grid.through(coord + North + West, coord + East)
+                .unwrap()
+                .map(|b| *b as usize)
+                .sum::<usize>()
+                - grid[coord] as usize
+        }
+        (_, _) => {
+            grid.through(coord + North + West, coord + South + East)
+                .unwrap()
+                .map(|b| *b as usize)
+                .sum::<usize>()
+                - grid[coord] as usize
+        }
+    }
+}
