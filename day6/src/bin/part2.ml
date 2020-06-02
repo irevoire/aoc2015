f3db7422312eef76dce63098df4ecadbba71053c
Thomas Campistron <irevoire@hotmail.fr>
2020-06-02 12:39:23 +0200

    [ocaml] add day6 part2

--- day6/src/bin/part2.ml
+++ day6/src/bin/part2.ml
@@ -7,9 +7,11 @@ let grid_update coord grid fn = grid_set coord grid (fn (grid_get coord grid))
 let apply_instruction grid instruction =
         let through = Coord.through instruction.from instruction.until in
         match instruction.action with
-        | Toggle -> Seq.iter (fun coord -> grid_update coord grid (not)) through
-        | TurnOn -> Seq.iter (fun coord -> grid_set coord grid true) through
-        | TurnOff -> Seq.iter (fun coord -> grid_set coord grid false) through
+        | Toggle -> Seq.iter (fun coord -> grid_update coord grid (fun a -> a + 2)) through
+        | TurnOff -> Seq.iter
+        (fun coord -> grid_update coord grid (fun a -> if a <= 0 then 0 else a - 1))
+        through
+        | TurnOn -> Seq.iter (fun coord -> grid_update coord grid (fun a -> a + 1)) through
 
 let apply_instructions grid instructions =
         List.iter (fun instr -> apply_instruction grid instr) instructions
@@ -18,15 +20,14 @@ let file = Sys.argv.(1)
 let file = open_in file
 let instructions = parse_orders file
 
-let grid = Array.make_matrix 1000 1000 false;;
+let grid = Array.make_matrix 1000 1000 0;;
 apply_instructions grid instructions;;
 
 let res = grid
         |> Array.map Array.to_seq
         |> Array.to_seq
         |> Iter.flatten
-        |> Seq.map (fun el -> if el then 1 else 0)
         |> Seq.fold_left (+) 0
 ;;
 
-Printf.printf "After following the instructions, %d lights are lit!\n" res;;
+Printf.printf "The total brightness of all lights combined is %d\n" res;;
